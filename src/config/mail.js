460f592c678564250e29d6c20d9941d3f8c355a8
Matheus Quintino <matheus.q@live.com>
2019-07-27 23:49:38 -0300

    almost there

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'abc3dda8da24b7',
+    pass: 'ce536486388b94',
+  },
+  default: {
+    from: 'Equipe BarberApp <noreply@barberapi.com',
+  },
+};
